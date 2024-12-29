101074e5db6735736ea398aea1db9bbe4d681c90
jjd214 <johnjacobdimaya0@gmail.com>
2024-12-29 12:32:15 +0800

    minor changes

--- resources/js/Pages/Approver/Approval/Index.vue
+++ resources/js/Pages/Approver/Approval/Index.vue
@@ -18,5 +18,15 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
                 > -->
             </div>
         </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <!-- Add any additional content here -->
+                </div>
+
+                <div class="my-4"></div>
+            </div>
+        </div>
     </AuthenticatedLayout>
 </template>
